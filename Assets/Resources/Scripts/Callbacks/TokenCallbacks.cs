44ca4c3ac73d92c1f9694c9158f9834a0d587f02
masonentrican <mason@entrican.com>
2018-12-06 01:12:13 -0800

    Jk I fixed tokens. but replicated location is fucked
    I dont know whats wrong but i need to sleep. It works but theres bugs with replication lol.

---
+++ Assets/Resources/Scripts/Callbacks/TokenCallbacks.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace com.dankstudios
+{
+    [BoltGlobalBehaviour]
+    public class TokenCallbacks : Bolt.GlobalEventListener
+    {
+        public override void BoltStartBegin()
+        {
+            BoltNetwork.RegisterTokenClass<TestToken>();
+        }
+    }
+}
